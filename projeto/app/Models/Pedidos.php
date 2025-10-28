a572523c8426298dd7f633241fd0feb8e7638009
12324-prog <12324@fai.com.br>
2025-10-28 20:05:00 -0300

    tabelas e triggers

--- projeto/app/Models/Pedidos.php
+++ projeto/app/Models/Pedidos.php
@@ -6,8 +6,6 @@
 
     class Pedidos extends Model {
 
-        public $cod_pedido;
-
         public $datahora;
 
         public $cod_cliente;
@@ -36,52 +34,103 @@
 
         public function listarPedidos(){
 
-            $listaPedidosDoBanco = DB::select('select * from pedidos order by cod_pedido DESC');
+            $listaPedidosDoBanco = DB::select('SELECT * FROM pedidos ORDER BY cod_pedido DESC');
 
             return $listaPedidosDoBanco;
 
         }
 
-        public function atualizarPedidos(){
-            //INCOMPLETO
-            $listaPedidosDoBanco = DB::update('update pedidos set nome_produto=,descricao_produto, preco');
+public function atualizarPedidos($id) {
+    return DB::update('UPDATE pedidos SET
+        datahora = ?,
+        cod_cliente = ?,
+        tipo_pedido = ?,
+        cod_entregador = ?,
+        valor_entrega = ?,
+        cod_mesa = ?,
+        encerrado = ?,
+        datahora_encerramento = ?,
+        desconto = ?,
+        pago = ?,
+        data_pago = ?,
+        taxa_servico = ?
+        WHERE cod_pedido = ?',
+        [
+            $this->datahora,
+            $this->cod_cliente,
+            $this->tipo_pedido,
+            $this->cod_entregador,
+            $this->valor_entrega,
+            $this->cod_mesa,
+            $this->encerrado ?? 0,
+            $this->datahora_encerramento,
+            $this->desconto ?? 0,
+            $this->pago ?? 0,
+            $this->data_pago,
+            $this->taxa_servico ?? 0,
+            $id
+        ]
+    );
+}
 
-            return $listaPedidosDoBanco;
-
-        } 
 
         public function buscarPedidos($cod_pedido){
 
-            $PedidosDoBanco = DB::select('select * from pedidos where cod_pedido = '.$cod_pedido);
+            $PedidosDoBanco = DB::select('SELECT * FROM pedidos WHERE cod_pedido = ?',[$cod_pedido]);
 
             return $PedidosDoBanco;
 
         }
 
-        public function gravar ($nmProduto,$descProduto, $precoProduto){
-
-            DB::insert('insert into produtos (nome_produto,descricao_produto, preco) values (?,?,?)', 
-        [ 
-            $cod_pedido, 
-            $datahora,
-            $cod_cliente,
-            $tipo_pedido,
-            $cod_entregador,
-            $valor_entrega,
-            $cod_mesa,
-            $encerrado ?? 0,
-            $datahora_encerramento,
-            $desconto ?? 0,
-            $pago,
-            $data_pago,
-            $valor_pago ?? 0,
-            $taxa_servico ?? 0]);
-            
+        public function gravar (){
+            DB::insert('INSERT INTO pedidos 
+                (datahora,
+                cod_cliente,
+                tipo_pedido,
+                cod_entregador,
+                valor_entrega,
+                cod_mesa,
+                encerrado,
+                datahora_encerramento,
+                desconto,
+                pago,
+                data_pago,
+                taxa_servico)
+                VALUES (?,?,?,?,?,?,?,?,?,?,?,?)', 
+                [ 
+                    $this->datahora = now(),
+                    $this->cod_cliente,
+                    $this->tipo_pedido,
+                    $this->cod_entregador,
+                    $this->valor_entrega,
+                    $this->cod_mesa,
+                    $this->encerrado ?? 0,
+                    $this->datahora_encerramento,
+                    $this->desconto ?? 0,
+                    $this->pago,
+                    $this->data_pago,
+                    $this->taxa_servico ?? 0
+                ]
+            );
         }
 
-        public function apagar ($codProduto)
+        public function apagar ($cod_pedido)
         {
-            DB::delete('delete from produtos where cod_produto = '.$codProduto);
+            DB::delete('DELETE FROM pedidos WHERE cod_pedido = ?', [$cod_pedido]);
+        }
+
+        public function trigger_apagar()
+        {
+            DB::unprepared('DROP TRIGGER IF EXISTS pedido_delete');
+
+            DB::unprepared('
+            CREATE TRIGGER pedido_delete
+            BEFORE DELETE ON pedidos
+            FOR EACH ROW
+            BEGIN
+                DELETE FROM itens_pedido 
+                WHERE cod_pedido = OLD.cod_pedido;
+            END');
         }
 
     }
